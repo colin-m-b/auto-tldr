7ba4724c1064a900ebe83c4150cd3529741fd410
Colin Brownlie <Colin Brownlie>
2016-07-06 23:53:34 -0700

    scrape w/arrays of a tags

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -2,6 +2,50 @@
 var body = $('body');
 console.log('scrape is running');
 
+function aTags() {
+	//create and populate array for article tags
+	var articleArr = [];
+	$("article a").each(function () {
+		var parentTag = $( this ).parent().get( 0 ).tagName;
+		var textStuff = $(this).text();
+		var obj = {
+			parent: parentTag,
+			href: this.href,
+			text: textStuff
+		}
+		articleArr.push(obj);
+	})
+
+	//create and populate array for section tags
+	var sectionArr = [];
+	$("section a").each(function () {
+		var parentTag = $( this ).parent().get( 0 ).tagName;
+		var textStuff = $(this).text();
+		var obj = {
+			parent: parentTag,
+			href: this.href,
+			text: textStuff
+		}
+		sectionArr.push(obj);
+	})
+
+	//create and populate array for footer tags
+	var footerArr = [];
+	$("footer a").each(function () {
+		var parentTag = $( this ).parent().get( 0 ).tagName;
+		var textStuff = $(this).text();
+		var obj = {
+			parent: parentTag,
+			href: this.href,
+			text: textStuff
+		}
+		footerArr.push(obj);
+	})
+	console.log('article length: ' + articleArr.length, "footer length: " + footerArr.length)
+}
+
+aTags();
+
 // test sending a message
 chrome.runtime.sendMessage({}, function(response) {
   console.log(response.farewell);
