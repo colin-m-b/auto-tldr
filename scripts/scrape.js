4a139f1795f07d281317e3b3046fbccfb8cd7f2c
Zhiwen Huang <zhiwen555@gmail.com>
2016-07-06 22:41:54 -0700

    popup and content script message sending and recieving

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,21 +1,8 @@
 // resize computerhope.com page size for the popup
-var html = document.getElementsByTagName("html");
-var width = 220;
+var body = $('body');
+console.log('scrape is running');
 
-function widthIncrease(timestamp) {
-  width += 65;
-  html[0].style.width = width + "px";
-  //if (width >= 600) clearInterval(intervalHandle);
-  if (width <= 600) {
-    window.requestAnimationFrame(widthIncrease);
-  }
-}
-
-window.requestAnimationFrame(widthIncrease);
-
-// box to cover page while its loading and resizing
-/*
-var newItem = document.createElement("div");
-var list = document.getElementsByTagName("h1");
-list.insertBefore(newItem, list.childNodes[0]);
-*/
+// test sending a message
+chrome.runtime.sendMessage({}, function(response) {
+  console.log(response.farewell);
+});
